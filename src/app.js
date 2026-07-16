498782727a22afe66357978707963940233dadad
Mahesh <maheshmsd18@gmail.com>
2026-07-16 10:34:27 +0530

    Multiple route-handlers

--- src/app.js
+++ src/app.js
@@ -1,40 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use('/test',(req, res)=> {
-    res.send('Test Server');
+app.use('/user',(req, res, next)=> {
+    // res.send('Response!!')
+    next();
+}, (req, res, next) => {
+    // res.send('2nd Response!!');
+    next();
+}, (req, res, next) => {
+    // res.send('3rd Response!!');
+    next();
+}, (req, res, next) => {
+    // res.send('4th Response!!');
+    next();
+}, (req, res, next) => {
+    res.send('5th Response!!');
+    
 });
 
 
-app.get('/user/:userId', (req, res) => {
-    console.log(req.params);
-    res.send({firstName: 'Mahesh', lastName: 'Reddy'});
-})
 
-app.get('/user', (req, res) => {
-    console.log(req.query)
-    res.send({firstName: 'Mahesh', lastName: 'Reddy'});
-});
-
-app.post('/user', (req, res) => {
-    res.send('User Data Saved Successfully');
-})
-
-app.patch('/user', (req, res) => {
-    res.send({firstName: 'Mahesh', lastName: 'Reddy', age: 26});
-});
-
-app.put('/user', (req, res) => {
-    res.send({ age: 26});
-});
-
-app.delete('/user', (req, res) => {
-    res.send('User Data Deleted Successfully');
-})
-
-app.use('/user', (req, res) => {
-    res.send('User Server');
-});
 
 app.listen(7777, () => {
     console.log('Server is running on port 7777');
