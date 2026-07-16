64ab161e86003f4e2c5177f3667c1b890e3cc0c3
Mahesh <maheshmsd18@gmail.com>
2026-07-16 09:39:01 +0530

    exploring http methods and playing with routes

--- src/app.js
+++ src/app.js
@@ -5,8 +5,35 @@ app.use('/test',(req, res)=> {
     res.send('Test Server');
 });
 
-app.use('/',(req, res)=> {
-    res.send('Hello From Server');
+
+app.get('/user/:userId', (req, res) => {
+    console.log(req.params);
+    res.send({firstName: 'Mahesh', lastName: 'Reddy'});
+})
+
+app.get('/user', (req, res) => {
+    console.log(req.query)
+    res.send({firstName: 'Mahesh', lastName: 'Reddy'});
+});
+
+app.post('/user', (req, res) => {
+    res.send('User Data Saved Successfully');
+})
+
+app.patch('/user', (req, res) => {
+    res.send({firstName: 'Mahesh', lastName: 'Reddy', age: 26});
+});
+
+app.put('/user', (req, res) => {
+    res.send({ age: 26});
+});
+
+app.delete('/user', (req, res) => {
+    res.send('User Data Deleted Successfully');
+})
+
+app.use('/user', (req, res) => {
+    res.send('User Server');
 });
 
 app.listen(7777, () => {
